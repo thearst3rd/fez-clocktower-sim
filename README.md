39d4610e4a59c0dfb5b07576362490ce4e5fc138
Terry Hearst <thearst3rd@gmail.com>
2021-12-12 21:57:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FEZ Clocktower Time Simulation
+
+This is a quick implementation of the FEZ clocktower in Love2D.
---
+++ main.lua
@@ -0,0 +1,21 @@
+-- FEZ clocktower time simulation in Love2D
+-- by thearst3rd
+
+
+--------------------
+-- MAIN CALLBACKS --
+--------------------
+
+function love.load()
+	-- todo
+end
+
+
+function love.update(dt)
+	-- todo
+end
+
+
+function love.draw()
+	-- todo
+end
